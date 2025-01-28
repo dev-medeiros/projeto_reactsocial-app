5027201736b251ab2ad56c8eef70e78ca8726cba
dev-medeiros <med.rodrigo97@gmail.com>
2025-01-28 07:21:16 -0300

    Base Backend com API e alguns testes

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+
+const port = process.env.PORT;
+
+const app = express();
+
+// configurar JSON and form data response
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// routes
+const router = require('./routes/Router.js');
+
+app.use(router);
+
+app.listen(port, () => {
+    console.log(`Server rodando na porta ${port}`);
+    });
